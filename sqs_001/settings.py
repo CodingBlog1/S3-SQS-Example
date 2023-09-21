9abb2c7ed13070dde1863015d1fb0fbea8081dfe
vipin.gupta <vipin.gupta@ongraph.com>
2023-09-21 11:26:32 +0530

    modified setting file

--- sqs_001/settings.py
+++ sqs_001/settings.py
@@ -113,20 +113,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# settings.py
-
-
-
-
-
-
-
-
-
-
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
